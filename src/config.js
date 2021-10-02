afb224a93a648dd0b5b060579d37882031eca0e1
JestemTK <91357627+Tiker2006@users.noreply.github.com>
2021-10-02 16:41:41 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "ODg5OTQ5ODkxMDgwNjI2MjQ2.YUosbA.0iB_i-sDLuw7tc1xrz4udUbXQKY",
+    token: "",
     dev: ["729459433779888230", "530805653468479500"],
     settings: {
         prefix: "."
